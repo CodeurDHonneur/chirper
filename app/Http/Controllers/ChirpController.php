ad957f227dda0e6cc2a9fe7c01e7d3f2be5b644b
KOUDJIWAN Kodjo Augustin <kkodjoaugustin@gmail.com>
2023-03-07 23:33:52 +0100

    chirper

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+use App\Models\chirp;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    /*public function index() 
+    {
+        //
+    }*/
+
+    public function index() : Response {
+        return response('Hello, World !');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(chirp $chirp)
+    {
+        //
+    }
+}
